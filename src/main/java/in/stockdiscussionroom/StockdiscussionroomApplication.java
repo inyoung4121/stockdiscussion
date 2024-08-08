81d6d4a7ddcba602250216ad543d961d1fa026b1
inyoung4121 <inyoungyang01@gmail.com>
2024-08-08 14:35:47 +0900

    start

---
+++ src/main/java/in/stockdiscussionroom/StockdiscussionroomApplication.java
@@ -0,0 +1,13 @@
+package in.stockdiscussionroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockdiscussionroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockdiscussionroomApplication.class, args);
+    }
+
+}
