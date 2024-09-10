5ac2fc6963772015fbefd11756874a849e9e88bd
inyoung4121 <inyoungyang01@gmail.com>
2024-09-10 13:17:36 +0900

    :fire:all

--- src/main/java/in/stockdiscussionroom/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package in.stockdiscussionroom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
